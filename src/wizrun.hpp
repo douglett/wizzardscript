1d00e7621134749a0883739d3e5c989067c10a10
Doug Inman <douglas.inman@live5.co.uk>
2024-09-01 23:34:55 +0100

    get variable name

--- src/wizrun.hpp
+++ src/wizrun.hpp
@@ -1,13 +1,20 @@
 #pragma once
 #include "node.hpp"
+#include <map>
 
 namespace WizRun {
 	using namespace std;
 	int rcall(const string& funcname);
+	int rsxpr(const Node& sx);
 
 	ostream* output = &cout;
 
 	Node program({});
+	map<string, int> mem;
+
+	int error(const string& msg) {
+		throw runtime_error(msg);
+	}
 
 	// reset memory state
 	int reset() {
@@ -24,28 +31,42 @@ namespace WizRun {
 		return 0;
 	}
 
+	// statement commands
 	int rprint(const Node& stmt) {
-		for (size_t i = 1; i < stmt.list.size(); i++)
-			*output << (i > 1 ? " " : "") << stmt[i].num;
+		for (size_t i = 1; i < stmt.list.size(); i++) {
+			*output << (i > 1 ? " " : "");
+			if    (stmt[i].type == Node::T_STRING)  cout << stmt[i].str;
+			else  cout << rsxpr( stmt[i] );
+		}
 		*output << endl;
 		return 0;
 	}
+	// int rblock() 
 
-	int rblock(const Node& block) {
-		for (size_t i = 1; i < block.list.size(); i++) {
-			auto& stmt = block.list[i];
-			if      (stmt.issx("print"))  rprint(stmt);
-			else    throw runtime_error("unknown statement in block: " + stmt.tostr());
+	int rsxpr(const Node& sx) {
+		if (sx.type == Node::T_NUMBER)  return sx.num;
+		if (!sx.issx())  return error("expected expression: " + sx.tostr());
+		auto& type = sx[0].str;
+
+		// statements
+		if (type == "block") {
+			for (size_t i = 1; i < sx.list.size(); i++)
+				rsxpr( sx[i] );
+			return 0;
 		}
+		else if (type == "print")  return rprint(sx);
+		// memory
+		else if (type == "get_global")  return mem[ sx[1].str ];
+		// expressions
+		// unknown
+		else  return error("unexpected expression: " + sx.tostr());
 		return 0;
 	}
 
 	int rcall(const string& funcname) {
 		for (const auto& func : program.list)
-			if (func.issx("function") && func[1].str == funcname) {
-				auto& block = func[3];
-				return rblock(block);
-			}
-		throw runtime_error("missing function: " + funcname);
+			if (func.issx("function") && func[1].str == funcname)
+				return rsxpr( func.findsx("block") );
+		return error("missing function: " + funcname);
 	}
 }
