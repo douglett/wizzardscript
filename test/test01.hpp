3fd806f427032a97386c8dc86dfd5e1dadf4b8c7
Doug Inman <douglas.inman@live5.co.uk>
2024-09-05 01:49:40 +0100

    new test case for basic strings

--- test/test01.hpp
+++ test/test01.hpp
@@ -18,5 +18,9 @@ struct Test01 : public UnitTest {
 		testno++;
 		runfile("03_int_variables.wizz");
 		expect( "testing int variables", ss.str() == "4\n23\n" );
+
+		testno++;
+		runfile("04_strings.wizz");
+		expect( "testing strings", ss.str() == "blah\nhello\n" );
 	}
 };
